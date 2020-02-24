85da7fc4777b621340843bb2a0a41110af5e4719
Shashwat Mehta <shashwat.mehta91@gmail.com>
2020-02-25 02:38:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/receiver/ReceiverApplication.java
@@ -0,0 +1,33 @@
+package com.example.receiver;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@SpringBootApplication
+public class ReceiverApplication {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		SpringApplication.run(ReceiverApplication.class, args);
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare("Assignment1", false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			System.out.println(" [x] Received '" + message + "'");
+		};
+		channel.basicConsume("Assignment1", true, deliverCallback, consumerTag -> { });
+	}
+
+}
